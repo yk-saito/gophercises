344159866d5a4ee068c51bf4a2e0d4836479a5d2
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-12-03 21:07:50 +0900

    feat: add exercise1, part1

---
+++ quiz/go.mod
@@ -0,0 +1,3 @@
+module yk-saito/quiz
+
+go 1.21.4
---
+++ quiz/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+func main() {
+	// 問題と回答が記載されたcsvファイルを読み込む
+	file, err := os.Open("problems.csv")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var answer string
+	counter := 0
+	passed := 0
+	reader := csv.NewReader(file)
+
+	for i := 0; ; i++ {
+		record, err := reader.Read()
+		// EOFに到達したらループを抜ける
+		if err == io.EOF {
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("Q%d: %s = ", i+1, record[0])
+		// ターミナルからの入力を受け取る
+		fmt.Scan(&answer)
+
+		// 正解数を数える
+		if answer == record[1] {
+			passed += 1
+		}
+		counter += 1
+	}
+
+	if passed == counter {
+		fmt.Printf("あなたは%d問全問正解しました! おめでとうございます🎉", counter)
+	} else {
+		fmt.Printf("あなたは%d問中%d問正解しました!", counter, passed)
+	}
+}
