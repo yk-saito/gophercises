b207bfb2d2445bc09e60c54c20d868d38d1c5f94
ysaito <60546319+yk-saito@users.noreply.github.com>
2023-12-09 21:29:46 +0900

    feat: add urlshort

---
+++ urlshort/urlshort/handler.go
@@ -0,0 +1,47 @@
+package urlshort
+
+import (
+	"net/http"
+)
+
+// MapHandler will return an http.HandlerFunc (which also
+// implements http.Handler) that will attempt to map any
+// paths (keys in the map) to their corresponding URL (values
+// that each key in the map points to, in string format).
+// If the path is not provided in the map, then the fallback
+// http.Handler will be called instead.
+func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		path := r.URL.Path
+
+		// リクエストされたパスがマップ何に存在するかを確認する
+		if dest, ok := pathsToUrls[path]; ok {
+			// パスが存在する場合、対応するURLへリダイレクトする
+			http.Redirect(w, r, dest, http.StatusFound)
+			return
+		}
+		// パスが存在しない場合、フォールバック関数を実行する
+		fallback.ServeHTTP(w, r)
+	}
+}
+
+// YAMLHandler will parse the provided YAML and then return
+// an http.HandlerFunc (which also implements http.Handler)
+// that will attempt to map any paths to their corresponding
+// URL. If the path is not provided in the YAML, then the
+// fallback http.Handler will be called instead.
+//
+// YAML is expected to be in the format:
+//
+//   - path: /some-path
+//     url: https://www.some-url.com/demo
+//
+// The only errors that can be returned all related to having
+// invalid YAML data.
+//
+// See MapHandler to create a similar http.HandlerFunc via
+// a mapping of paths to urls.
+func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
+	// TODO: Implement this...
+	return nil, nil
+}
